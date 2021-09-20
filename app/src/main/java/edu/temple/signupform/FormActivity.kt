91920950663d28fa89faa10447f7eda4d1b4f32f
Alyssa <tui91984@temple.edu>
2021-09-20 14:53:28 -0400

    Added errors for blank input fields and matching password fields, make toast for successful login

--- app/src/main/java/edu/temple/signupform/FormActivity.kt
+++ app/src/main/java/edu/temple/signupform/FormActivity.kt
@@ -2,10 +2,50 @@ package edu.temple.signupform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class FormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //get views for input and button
+
+        val name = findViewById<EditText>(R.id.name)
+        val email = findViewById<EditText>(R.id.email)
+        val password = findViewById<EditText>(R.id.password)
+        val passwordCheck = findViewById<EditText>(R.id.passwordConfirmation)
+
+        val saveButton = findViewById<Button>(R.id.saveButton)
+
+        //create event listener for save button
+
+        saveButton.setOnClickListener{
+
+            //make sure every field has data entered
+            if(name.text.isBlank()){
+                name.error = "Please fill out the name field."
+            } else if(email.text.isBlank()){
+                email.error = "Please fill out the email field."
+            } else if(password.text.isBlank()){
+                password.error = "Please fill out the password field."
+            } else if(passwordCheck.text.isBlank()){
+                passwordCheck.error = "Please fill out the password field."
+            }
+
+            //make sure the password and password confirmation match
+            else if(!password.text.toString().equals(passwordCheck.text.toString())) {
+                Toast.makeText(this@FormActivity, "Please make sure password and password confirmation match.", Toast.LENGTH_LONG).show()
+            }
+
+            //no black fields, passwords match
+            else {
+                Toast.makeText(this@FormActivity, "Welcome, " + name.text + " to the SignUpForm App!", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
